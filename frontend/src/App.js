f97e8bc611b65224ea8b03dd3ea1c227d60cb2f2
DEEPAK <129509561+Deepakk2004@users.noreply.github.com>
2025-02-15 01:13:30 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import AutoStake from "./components/AutoStake";
+
+function App() {
+  return (
+    <div>
+      <h1>AutoStake AI</h1>
+      <AutoStake />
+    </div>
+  );
+}
+
+export default App;
